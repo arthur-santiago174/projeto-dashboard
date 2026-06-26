1e4bfe651e825675293902094dc865e9d744dc62
peter parker <parker@MacBook-Air-de-daniel.local>
2026-06-25 23:52:58 -0300

    Primeiro commit

---
+++ public/index.js
@@ -0,0 +1,182 @@
+const API_URL = 'http://localhost:3007/tasks';
+
+const taskForm = document.getElementById('taskForm')
+const pendingTasks = document.getElementById('pendingTasks')
+const completedTasks = document.getElementById('completedTasks')
+
+const pendingTab = document.getElementById('pendingTab')
+const completedTab = document.getElementById('completedTab')
+const pendingPanel = document.getElementById('pendingPanel')
+const completedPanel = document.getElementById('completedPanel')
+
+const pendingCount = document.getElementById('pendingCount');
+const completedCount = document.getElementById('completedCount');
+
+// CARREGAR TAREFAS 
+async function loadTasks() {//Essa função serve para buscar as tarefas no backend e mostrar na tela
+    pendingTasks.innerHTML = '';
+    completedTasks.innerHTML = '';
+
+    const res = await fetch(API_URL);
+    const tasks = await res.json();
+
+
+    let totalPending = 0;
+    let totalCompleted = 0;
+
+    tasks.forEach(task => {
+        const li = document.createElement('li')
+        li.className = task.completed ? 'completed' : "";
+        li.innerHTML = `
+            <span>${task.description}</span>
+            <div class="tarefas">
+                ${task.completed
+                ?
+                `<button class="action toggle" onclick="toggleTask(${task.id}, ${task.completed})"> Reabrir </button>
+                    <button class="action delete" onclick="deleteTask(${task.id})"> Excluir </button>`
+                :
+                `<button class="action toggle" onclick="toggleTask(${task.id}, ${task.completed})"> Concluir </button>
+                    <button class="action edit" onclick="editTask(${task.id}, '${task.description}')"> Editar </button>
+                    <button class="action delete" onclick="deleteTask(${task.id})"> Excluir </button>`
+            }
+            </div>
+        `;
+
+        if (task.completed) {
+            totalCompleted++
+            completedTasks.appendChild(li);
+        } else {
+            totalPending++
+            pendingTasks.appendChild(li)
+        }
+    });
+    pendingCount.textContent = totalPending;
+    completedCount.textContent = totalCompleted;
+
+}
+
+
+//adicionar tarefa
+taskForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const description = document.getElementById('description').value.trim(); // o que o usuario digitou
+
+    if (!description) return;
+
+    await fetch(API_URL, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' }, // Estou enviando dados em formato JSON.
+        body: JSON.stringify({ description })
+    })
+
+    taskForm.reset();
+    loadTasks()
+})
+
+// editar tasks
+async function editTask(id, currentDescription) {
+    const modal = document.createElement('div');
+    modal.className = 'edit-modal-overlay';
+
+    modal.innerHTML = `
+        <div class="edit-modal">
+            <h2>Editar tarefa</h2>
+        <input type="text" id="editDescription" placeholder="Digite a nova descrição">
+            <div class="edit-modal-actions">
+                <button class="cancel-edit" type="button">Cancelar</button>
+                <button class="save-edit" type="button">Salvar</button>
+            </div>
+        </div>
+    `;
+
+    document.body.appendChild(modal);
+
+    const editInput = document.getElementById('editDescription');
+    const cancelButton = modal.querySelector('.cancel-edit');
+    const saveButton = modal.querySelector('.save-edit');
+
+    editInput.focus();
+
+    cancelButton.addEventListener('click', () => {
+        modal.remove();
+    });
+
+    saveButton.addEventListener('click', async () => {
+        const newDescription = editInput.value.trim();
+
+        if (!newDescription) return;
+
+        await fetch(`${API_URL}/${id}`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ description: newDescription })
+        });
+
+        modal.remove();
+        loadTasks();
+    });
+}
+
+// deletar tasks
+async function deleteTask(id) {
+    const modal = document.createElement('div');
+    modal.className = 'delete-modal-overlay';
+
+    modal.innerHTML = `
+        <div class="delete-modal">
+            <h2>Excluir tarefa</h2>
+            <p>Tem certeza que deseja excluir esta tarefa?</p>
+            <div class="delete-modal-actions">
+                <button class="cancel-delete" type="button">Cancelar</button>
+                <button class="confirm-delete" type="button">Excluir</button>
+            </div>
+        </div>
+    `;
+
+    document.body.appendChild(modal);
+
+    const cancelButton = modal.querySelector('.cancel-delete');
+    const confirmButton = modal.querySelector('.confirm-delete');
+
+    cancelButton.addEventListener('click', () => {
+        modal.remove();
+    });
+
+    confirmButton.addEventListener('click', async () => {
+        await fetch(`${API_URL}/${id}`, {
+            method: 'DELETE'
+        });
+
+        modal.remove();
+        loadTasks();
+    });
+}
+
+//Reabrir
+async function toggleTask(id, completed) {
+    await fetch(`${API_URL}/${id}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ completed: !completed })
+    });
+
+    loadTasks();
+}
+
+pendingTab.addEventListener('click', () => {
+    pendingTab.classList.add('active');
+    completedTab.classList.remove('active');
+
+    pendingPanel.classList.add('active');
+    completedPanel.classList.remove('active');
+});
+
+completedTab.addEventListener('click', () => {
+    completedTab.classList.add('active');
+    pendingTab.classList.remove('active');
+
+    completedPanel.classList.add('active');
+    pendingPanel.classList.remove('active');
+});
+
+loadTasks();
---
+++ server.js
@@ -0,0 +1,130 @@
+//Recebe pedidos do front-end
+// ↓
+// Entende se é criar, listar, editar ou excluir
+// ↓
+// Conecta no MySQL
+// ↓
+// Executa SQL
+// ↓
+// Devolve resposta para o script.js
+
+
+
+const express = require('express')
+const mysql = require('mysql2/promise');
+
+const app = express()
+const PORT = 3007;
+
+const path = require('path') //Ele serve para trabalhar com caminhos de arquivos e pastas.
+app.use(express.static(path.join(__dirname, 'public'))); //Serve para: fazer o Express entregar os arquivos da pasta public.
+app.use(express.json()); //Serve para: permitir que o backend leia JSON enviado pelo front.
+
+//config da conexao do banco de dados
+const dbConfig = {
+    host: 'localhost',
+    user: 'root',
+    password: '123456',
+    database: 'task_dashboard'
+};
+
+
+// Criar tarefa
+app.post('/tasks', async (req, res) => {
+    try {
+        const connection = await mysql.createConnection(dbConfig);
+        const { description } = req.body;
+
+        const [result] = await connection.execute(
+            'INSERT INTO tasks (description) VALUES (?)',
+            [description]
+        );
+
+        await connection.end();
+
+        res.status(201).json({
+            id: result.insertId,
+            description,
+            completed: false
+        });
+
+    } catch (error) {
+        console.error('erro ao criar tarefa', error);
+        res.status(500).json({ error: 'erro interno do servidor' });
+    }
+})
+
+
+
+
+// Listar tarefas
+app.get('/tasks', async (req, res) => {
+    try {
+        const connection = await mysql.createConnection(dbConfig);
+        const [rows] = await connection.execute(
+            'SELECT * FROM tasks ORDER BY id DESC'
+        );
+        await connection.end();
+        res.json(rows)
+    } catch (error) {
+        console.error('Erro ao listar tarefas:', error);
+        res.status(500).json({ error: 'erro interno do servidor' });
+    }
+});
+
+
+//configurando o put
+app.put('/tasks/:id', async (req, res) => {
+    try {
+        const connection = await mysql.createConnection(dbConfig);
+        const { id } = req.params;
+        const { description, completed } = req.body;
+
+        const [currentTask] = await connection.execute(
+            'SELECT * FROM tasks WHERE id = ?',
+            [id]
+        );
+
+        if (currentTask.length === 0) {
+            await connection.end();
+            return res.status(404).json({ error: 'tarefa não encontrada' });
+        }
+
+        const newDescription = description ?? currentTask[0].description;
+        const newCompleted = completed ?? currentTask[0].completed;
+
+        await connection.execute(
+            'UPDATE tasks SET description = ?, completed = ? WHERE id = ?',
+            [newDescription, newCompleted, id]);
+        await connection.end();
+        res.json({ id, description: newDescription, completed: newCompleted });
+    } catch (error) {
+        console.error('Erro ao editar tarefa:', error);
+        res.status(500).json({ error: 'erro interno do servidor' });
+    }
+})
+
+
+//configurando o delete
+app.delete('/tasks/:id', async (req, res) => {
+    try {
+        const connection = await mysql.createConnection(dbConfig);
+        const { id } = req.params;
+        await connection.execute(
+            'DELETE FROM tasks WHERE id = ?',
+            [id]
+        );
+        await connection.end();
+        res.json({ message: 'tarefa excluída com sucesso' });
+    } catch (error) {
+        console.error('Erro ao excluir tarefa:', error);
+        res.status(500).json({ error: 'erro interno do servidor' });
+    }
+})
+
+
+
+//configuração do servidor (porta)
+app.listen(PORT, () => {
+    console.log(`servidor rodando em http://localhost:${PORT}`)
+})
